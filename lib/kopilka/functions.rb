b75e1183b2b93680c6358fa8866761ed34ab772f
Nerbyk <danil.maximov@flatstack.com>
2022-07-02 02:16:32 +0200

    intial commit

---
+++ lib/kopilka/functions.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "functions/version"
+
+module Kopilka
+  module Functions
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
