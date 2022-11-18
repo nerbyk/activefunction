fb3bd90431bc4525716cdd0e9e200b9b2cac1cb9
Nerbyk <danil.maximov@flatstack.com>
2022-11-18 18:06:55 +0100

    refactor logger

--- lib/active_function/logger.rb
+++ lib/active_function/logger.rb
@@ -1,23 +1,26 @@
 require "logger"
 
 module ActiveFunction
-  Logger = Object.new
-  Logger.instance_eval do
-    def error(err)
-      logger.error(err)
-    end
-
-    def info(msg)
-      logger.info(msg)
-    end
-
-    def logger
-      @logger ||= ::Logger.new($stdout).tap do |l|
-        l.level = ::Logger::INFO
-        l.formatter = proc do |severity, datetime, _progname, msg|
-          "#{datetime.strftime("%Y-%m-%d %H:%M:%S")} #{severity} #{msg}\n"
+  class Logger
+    class << self
+      def error(err)
+        logger.error(err)
+      end
+  
+      def info(msg)
+        logger.info(msg)
+      end
+  
+      def logger
+        @logger ||= ::Logger.new($stdout).tap do |l|
+          l.level = ::Logger::INFO
+          l.formatter = proc do |severity, datetime, _progname, msg|
+            "#{datetime.strftime("%Y-%m-%d %H:%M:%S")} #{severity} #{msg}\n"
+          end
         end
       end
+  
+      private_class_method :logger
     end
   end
 end
