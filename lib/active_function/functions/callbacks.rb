8d37a96549cb2821797e63ccc7f4e4468bd803fb
Nerbyk <danil.maximov@flatstack.com>
2022-11-18 18:05:02 +0100

    refactor callbacks module

--- lib/active_function/functions/callbacks.rb
+++ lib/active_function/functions/callbacks.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
-module ActiveFunction::Functions
-  module Callbacks
-    CALLBACK_FILTER_TYPES = %i[before after].freeze
+module ActiveFunctionFunctions
+  class MiggingCallbackContext < Error # :no_doc:
+    MESSAGE_TEMPLATE = "Missing callback context: %s"
 
-    def self.included(base)
-      base.extend(ClassMethods)
+    attr_reader :message
+
+    def initialize(context)
+      @message = MESSAGE_TEMPLATE % context
     end
+  end  
 
-    def run_callbacks
-      _run_callbacks(:before)
+  module Callbacks # :nodoc:
 
-      yield if block_given?
-
-      _run_callbacks(:after)
+    class << self
+      def included(base)
+        base.extend(ClassMethods)
+      end
     end
 
     def process(*)
@@ -22,33 +25,42 @@ module ActiveFunction::Functions
       end
     end
 
-    private 
+    private
 
-    def _run_callbacks(type)
-      self.class.__callbacks[type].each do |callback_method, filters|
+    def run_callbacks
+      exec_callbacks(:before)
+
+      yield if block_given?
+
+      exec_callbacks(:after)
+    end
+
+    def exec_callbacks(type)
+      self.class.callbacks[type].each do |callback_method, filters|
+        raise MiggingCallbackContext, callback_method unless respond_to?(callback_method)
+
         public_send(callback_method) if filters[:if][action_name]
       end
     end
 
-    module ClassMethods
-      self.__callbacks = {
-        before: {},
-        after: {}
-      }
+    module ClassMethods # :nodoc:
+      CALLBACKS = { before: {}, after: {} }.freeze
 
       def before_action(method_name, **options)
-        normalized_options = normalize_options(options)
-        set_callback(:before, method_name, normalized_options)
+        set_callback(:before, method_name, filter(options))
       end
 
       def after_action(method_name, **options)
-        normalized_options = normalize_options(options)
-        set_callback(:after, method_name, normalized_options)
+        set_callback(:after, method_name, filter(options))
       end
 
-      private
+      def callbacks
+        return @callbacks if instance_variable_defined?(:@callbacks)
 
-      def normalize_options(options)
+        @callbacks = CALLBACKS
+      end
+
+      def filter(options)
         if only_list = options[:only]
           options[:if] = proc { |action| only_list.map(&:to_s).include?(action) }
         end
@@ -57,10 +69,10 @@ module ActiveFunction::Functions
       end
 
       def set_callback(type, method_name, filters)
-        return unless CALLBACK_FILTER_TYPES.include?(type)
-
-        __callbacks[type][method_name] = filters
+        callbacks[type][method_name] = filters
       end
+
+      private :filter, :set_callback
     end
-  end 
+  end
 end
