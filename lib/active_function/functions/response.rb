02b2dda52fbff0e869c2f9c3b67b225a0f26c18e
nerbyk <danil.maximov2000@gmail.com>
2023-01-08 17:24:59 +0100

    add response bouilder class

---
+++ lib/active_function/functions/response.rb
@@ -0,0 +1,21 @@
+module ActiveFunction
+  module Functions
+    class Response
+      attr_accessor :status, :headers, :body
+
+      def initialize(status: 200, headers: {}, body: nil)
+        @status     = status
+        @headers    = headers
+        @body       = body
+      end
+
+      def to_h
+        {
+          statusCode: status,
+          headers:    headers,
+          body:       body
+        }
+      end
+    end
+  end
+end
