bbc88a6b93050bdee1ea56fc7e469c971b944811
Nerbyk <danil.maximov@flatstack.com>
2023-12-27 14:54:54 +0700

    Revert "Add `ruby/spec` Data specs"
    This reverts commit 01c3ba7d26a7780e00592783f51ce797833c4e12.

--- gems/activefunction-core/test/active_function_core/polyfills/data/fixtures/classes.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Source: https://github.com/ruby/spec/blob/master/core/data/fixtures/classes.rb
-
-module DataSpecs
-  ruby_version_is "3.2" do
-    Measure = Data.define(:amount, :unit)
-  end
-end
