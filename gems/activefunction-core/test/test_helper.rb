ecdd8b7e47eb1d302391c95e3a510fa86c57ba40
Nerbyk (Danil Maksimov) <danil.maximov2000@gmail.com>
2024-01-14 18:36:55 +0700

    fix ci

--- gems/activefunction-core/test/test_helper.rb
+++ gems/activefunction-core/test/test_helper.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 require "bundler/setup"
-require "ruby-next/language/runtime" unless ENV["CI"]
+require "ruby-next/language/runtime"
 
 require "minitest/autorun"
 require "minitest/reporters"
 
 require "active_function"
-RubyNext::Language.setup_gem_load_path("test", transpile: true)
 
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new(color: true)]
